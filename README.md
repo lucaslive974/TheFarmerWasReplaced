9eeaddf0dcf5e5b3bd6b2493fa0f8271b92cff28
lucaslive974 <lucasribeirolima974@gmail.com>
2026-09-12 22:48:53 -0300

    [chore]: Add .gitignore and README;
    update main.py and remove save.json

---
+++ README.md
@@ -0,0 +1,8 @@
+# The Farmer Was Replaced
+
+My algorithms for the steam programming gaming
+
+## To-Do
+
+- Priority algorithm for seeds
+- Scaling system for multiple seeds managing
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from Iterator import *
 
 def harvest_if_possible() -> None:
         while not can_harvest():
+            do_a_flip()
             continue
         harvest()
 
 def engine_start() -> None:
     clear()  
     customize_drone()   
-    it = get_iterator(5, [0, 0])
+    it = get_iterator(6, [0, 0])
     while True:
         traverse(plant_bush, reverse_iterator(it))
         traverse(harvest_if_possible, it)
